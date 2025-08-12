5de148e7e2245bc2f969c6bb3592e44c7a857802
Sanchit Hajela <sanchit.hajela@skypointcloud.com>
2025-08-12 18:03:37 +0530

    chore: unignore root JS configs; add ESLint flat config

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+import js from '@eslint/js';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+
+export default [
+  {
+    ignores: ['dist/**', 'node_modules/**']
+  },
+  js.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        sourceType: 'module',
+        ecmaVersion: 2022
+      }
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin
+    },
+    rules: {
+      // Prefer TS rule over base rule
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      'no-undef': 'off'
+    }
+  }
+];
+
